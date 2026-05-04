04fe56cdb27b99d384a515ca47fd574590fd8d7e
jaguar178 <j_j_22@web.de>
2026-05-04 14:33:14 +0200

    Update predict.py

--- utils/predict.py
+++ utils/predict.py
@@ -1,1 +1,25 @@
+import tensorflow as tf
+import numpy as np
+import os
 
+# absoluter Pfad (wichtig!)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "..", "model", "keras_model.h5")
+
+if not os.path.exists(MODEL_PATH):
+    raise FileNotFoundError(f"Model NOT found at: {MODEL_PATH}")
+
+model = tf.keras.models.load_model(MODEL_PATH, compile=False)
+
+def predict_image(image):
+    # Beispiel-Preprocessing (Teachable Machine kompatibel)
+    image = image.resize((224, 224))
+    img_array = np.array(image) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    preds = model.predict(img_array)
+    
+    confidence = float(np.max(preds))
+    label_index = int(np.argmax(preds))
+
+    return label_index, confidence
